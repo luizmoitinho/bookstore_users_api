9af4c90713315ad030ca1f85b3ba21e8b2b78e72
luizmoitinho <luiz.moitinho@olxbr.com>
2022-11-19 22:52:08 -0300

    feat: add config and env vars

---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var Propertie Properties
+
+type Properties struct {
+	ENV       string
+	CONTAINER bool
+	PORT      int
+	DATABASE  DataBase
+}
+
+type DataBase struct {
+	HOST       string
+	PORT       string
+	NAME       string
+	USER       string
+	PASS       string
+	COLLECTION string
+}
+
+func Load(path string) {
+	var err error
+
+	if err = godotenv.Load(path); err != nil {
+		log.Fatal("config.Load(): ", err)
+	}
+
+	Propertie.ENV = os.Getenv("ENV")
+	if Propertie.PORT, err = strconv.Atoi(os.Getenv("PORT")); err != nil {
+		Propertie.PORT = 8080
+		log.Printf("cannot convert PORT at .env: %v", err)
+	}
+
+	//mysql
+	Propertie.DATABASE.HOST = os.Getenv("DB_HOST")
+	Propertie.DATABASE.PORT = os.Getenv("DB_PORT")
+	Propertie.DATABASE.NAME = os.Getenv("DB_NAME")
+	Propertie.DATABASE.USER = os.Getenv("DB_USER")
+	Propertie.DATABASE.PASS = os.Getenv("DB_PASS")
+	Propertie.DATABASE.COLLECTION = os.Getenv("DB_COLLECTION")
+
+}
